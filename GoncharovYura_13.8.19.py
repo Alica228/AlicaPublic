e24857ac22b8f64bf79c83aed6334fcd12dc81c3
Yura <Yura25051999@yandex.ru>
2020-12-23 10:49:22 +0200

    Task 13.8.19

---
+++ GoncharovYura_13.8.19.py
@@ -0,0 +1,46 @@
+import pymorphy2
+# заранее определяем валюту, в которой будем проводить расчёт
+morph = pymorphy2.MorphAnalyzer()
+RUB = morph.parse('рубль')[0]
+
+def Price(age):
+    """
+    Функция вывода цены для конкретного возраста, прайс задаётся вручную
+
+    Input:
+    age -- возраст человека
+
+    Output:
+    price -- цена для конкретного возраста
+
+    Keyword arguments:
+    age -- int 
+
+    """
+    return {age < 18        : 0,
+            18 <= age < 25  : 990,
+            25 < age        : 1390
+        }[True]
+
+while True:
+    try:
+        # считываем количество билетов
+        numberOfTickets = int(input("Введите количество билетов: "))
+        break
+    except ValueError:
+        print("Требуется число!\n")
+
+while True:
+    try:
+        # считываем возраст для каждого из билетов
+        agesForAllTickets = [int(input(f"Введите возраст для билета #{i+1}: ")) for i in range(numberOfTickets)]
+        break
+    except ValueError:
+        print("Требуется число!\n")
+
+# считаем сумму ориентируясь на прайс, если количество билетов больше, либо равно 3 -> делаем скидку 10%
+summa = sum([Price(age) for age in agesForAllTickets])
+if numberOfTickets >= 3:
+    summa *= 0.9
+
+print(f"Итоговая сумма: {summa:.0f} {RUB.make_agree_with_number(summa).word}")
